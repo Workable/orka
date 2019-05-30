663ca12694b1ed0e1cd1be9cce75d9bb6065f1cf
Alkis Kalogeris <alkis.kalogeris@gmail.com>
2019-05-30 17:13:34 +0300

    vscode and test

--- examples/simple-example/routes.js
+++ examples/simple-example/routes.js
@@ -1,5 +1,14 @@
 module.exports = {
   get: {
-    '/test': async (ctx, next) => (ctx.body = 'ok')
+    '/test': async (ctx, next) => (ctx.body = 'ok'),
+    '/testPolicy': async (ctx, next) => (ctx.body = 'ok')
+  },
+  policy: {
+    '/testPolicy': async (ctx, next) => {
+      if(ctx.request.query.secret_key === 'success') {
+        return await next();
+      }
+      throw { status: 401, message: 'Unauthorized' };
+    }
   }
 };
