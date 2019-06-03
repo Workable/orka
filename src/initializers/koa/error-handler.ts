e79d56cb04b89e8bc0388af65affce2f8f071cbe
Nikos Kostoulas <nikostoulas@users.noreply.github.com>
2019-06-03 13:35:47 +0300

    Update fast-koa-router, use matchedRoutes, update builder (#25)

--- src/initializers/koa/error-handler.ts
+++ src/initializers/koa/error-handler.ts
@@ -16,7 +16,7 @@ export default config => async (ctx: Koa.Context, next: () => Promise<any>) => {
 
     Object.assign(err, {
       component: err.component || 'koa',
-      action: err.action || ctx.request.path,
+      action: err.action || ctx._matchedRoute || ctx.request.path,
       params: {
         ...ctx.request.query,
         requestId: ctx.state.requestId,
