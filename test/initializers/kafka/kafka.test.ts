30a7eab3f19c29e6f3303f88aa8310cce2e39360
Alkis Kalogeris <alkis.kalogeris@gmail.com>
2019-06-22 23:26:05 +0300

    add kafka class test

---
+++ test/initializers/kafka/kafka.test.ts
@@ -0,0 +1,41 @@
+import * as sinon from 'sinon';
+import 'should';
+import Kafka from '../../../src/initializers/kafka/kafka';
+import * as tmp from 'tmp';
+import * as fs from 'fs';
+
+const sandbox = sinon.sandbox.create();
+
+describe('kafka class', () => {
+  const producerStub = { connect: sandbox.stub(), on: sandbox.stub(), send: sandbox.stub().returns({}) };
+  const consumeStub = {};
+  before(() => {
+    sandbox.stub(fs, 'writeFileSync');
+    sandbox.stub(tmp, 'fileSync').returns({ name: 'tmpkey' });
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it('should call correct methods with correct args', async () => {
+    sandbox.stub(Kafka.prototype, 'createProducer').returns(producerStub);
+    sandbox.stub(Kafka.prototype, 'createConsumer').returns(consumeStub);
+    const kafka = new Kafka({
+      key: 'key',
+      cert: 'cert',
+      ca: 'ca',
+      groupId: 'groupId',
+      clientId: 'clientId',
+      brokers: []
+    });
+    await kafka.connect();
+    producerStub.connect.calledOnce.should.eql(true);
+    producerStub.on.calledOnce.should.eql(true);
+    producerStub.connect.calledOnce.should.eql(true);
+    producerStub.on.calledWith('error').should.eql(true);
+
+    await kafka.send('topic', 'msg');
+    producerStub.send.calledWith('topic', 'msg').should.eql(true);
+  });
+});
