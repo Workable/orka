c7e00eaaee97f338e634561118194ed85b99f6e4
Nikos Kostoulas <kostoulas@workable.com>
2019-07-16 18:46:23 +0300

    Optional peer dependencies

--- src/require-injected.ts
+++ src/require-injected.ts
@@ -1,2 +1,2 @@
 import * as codependency from 'codependency';
-export default codependency.register(module, { strictCheck: false, index: ['optionalDependencies'] });
+export default codependency.register(module, { strictCheck: false, index: ['optionalPeerDependencies'] });
