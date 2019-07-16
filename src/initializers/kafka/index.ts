aa3ae88f7614b7108c853b4204348dd8b6908778
Nikos Kostoulas <kostoulas@workable.com>
2019-07-16 18:58:20 +0300

    Require sinek only when kafkaconfig.brokers are given

--- src/initializers/kafka/index.ts
+++ src/initializers/kafka/index.ts
@@ -1,5 +1,4 @@
 import { isEmpty } from 'lodash';
-import { default as Kafka } from './kafka';
 import { KafkaConfig } from '../../typings/kafka';
 
 export { default as BaseKafkaHandler } from './base-kafka-handler';
@@ -7,6 +6,8 @@ export { default as BaseKafkaHandler } from './base-kafka-handler';
 let kafka;
 export default async (kafkaConfig: KafkaConfig) => {
   if (!isEmpty(kafkaConfig && kafkaConfig.brokers)) {
+    const Kafka = (await import('./kafka')).default;
+
     kafka = new Kafka(kafkaConfig);
     await kafka.connect();
   }
