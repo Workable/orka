f04542677c30e317dd7e0caee34f5767a1b28313
Nikos Kostoulas <nikostoulas@users.noreply.github.com>
2019-08-01 13:25:01 +0300

    Bump rabbit and export it. (#61)

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import Orka from './orka';
 import * as Koa from 'koa';
+import * as RabbitQueue from 'rabbit-queue';
 import * as mongoose from 'mongoose';
 
 export { default as builder } from './builder';
 export { getLogger } from './initializers/log4js';
 export * from './initializers/kafka';
 export const orka = config => Orka(config);
-export { Koa };
 export { getNewRelic } from './initializers/newrelic';
 export { getKafka } from './initializers/kafka';
 export { getRabbit } from './initializers/rabbitmq';
-export { mongoose };
+export { Koa, RabbitQueue, mongoose };
