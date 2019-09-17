484779a9de3899379f3a6448137e89ff6f02c018
Alkis Kalogeris <alkis.kalogeris@gmail.com>
2019-09-17 16:58:04 +0300

    two steps orka init

--- src/orka.ts
+++ src/orka.ts
@@ -2,9 +2,10 @@ import * as lodash from 'lodash';
 import builder from './builder';
 import defaults from './default-options';
 import { OrkaOptions } from './typings/orka';
+import OrkaBuilder from './orka-builder';
 
-const fromOptions = (options: Partial<OrkaOptions>) => {
-  return builder(lodash.defaultsDeep(options, defaults))
+const fromOptions = (options: Partial<OrkaOptions & { builder?: OrkaBuilder }>) => {
+  return (options.builder || builder(lodash.defaultsDeep(options, defaults)))
     .forTypescript(options.typescript)
     .use(options.beforeMiddleware)
     .useDefaults()
