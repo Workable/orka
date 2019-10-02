408ee3bc5920d29e22630bddc50699778e25f67c
Manos Kouvarakis <kouvarakis@workable.com>
2019-10-02 16:28:52 +0300

    fixup! Implement json appender for log4js

--- src/initializers/log4js/json-appender.ts
+++ src/initializers/log4js/json-appender.ts
@@ -1,6 +1,6 @@
 import * as lodash from 'lodash';
 
-const jsonAppender = ({ filter_status = [] }) => {
+const jsonAppender = () => {
   return logEvent => {
     let event = logEvent.level.levelStr === 'ERROR' ? createErrorLog(logEvent) : createValidLog(logEvent);
     var json = JSON.stringify(event);
@@ -9,20 +9,34 @@ const jsonAppender = ({ filter_status = [] }) => {
 };
 
 const createErrorLog = logEvent => {
+  const data = lodash.flattenDeep(logEvent.data);
+  let context = {};
+  data.forEach(element => {
+    if (typeof element === 'object') {
+      Object.assign(context, element);
+    }
+  });
+
   return {
     timestamp: logEvent.startTime,
     severity: logEvent.level.levelStr,
     categoryName: logEvent.categoryName,
     message: logEvent.data[0].message,
-    stack_trace: logEvent.data[0].stack
+    stack_trace: logEvent.data[0].stack,
+    context
   };
 };
 
 const createValidLog = logEvent => {
   const data = lodash.flattenDeep(logEvent.data);
+  let context = {};
   const message = data
     .map(element => {
-      return typeof element === 'object' ? JSON.stringify(element) : element;
+      if (typeof element === 'object') {
+        Object.assign(context, element);
+      } else {
+        return element;
+      }
     })
     .join(' ');
 
@@ -30,13 +44,11 @@ const createValidLog = logEvent => {
     timestamp: logEvent.startTime,
     severity: logEvent.level.levelStr,
     categoryName: logEvent.categoryName,
-    message: message
+    message: message,
+    context
   };
 };
 
-export function configure(config = {} as any) {
-  const { filter_status = [] } = config;
-  return jsonAppender({
-    filter_status
-  });
+export function configure() {
+  return jsonAppender();
 }
