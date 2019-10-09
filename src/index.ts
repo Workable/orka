3935b18d778834e10eb1880af221a3f6d609976a
Nikos Kostoulas <kostoulas@workable.com>
2019-10-09 18:38:30 +0300

    Fix typings of orka function

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import Orka from './orka';
 export { default as builder } from './builder';
 export { getLogger } from './initializers/log4js';
 export * from './initializers/kafka';
-export const orka = config => Orka(config);
+export const orka = Orka;
 export { getNewRelic } from './initializers/newrelic';
 export { getKafka } from './initializers/kafka';
 export { getRabbit } from './initializers/rabbitmq';
