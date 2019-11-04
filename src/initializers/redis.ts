3132baa9829e633f623a5b003b683cf4d6947f6b
Nikos Kostoulas <kostoulas@workable.com>
2019-11-04 18:02:27 +0100

    Teach orka redis

---
+++ src/initializers/redis.ts
@@ -0,0 +1,58 @@
+import { createClient, RedisClient } from 'redis';
+import { getLogger } from './log4js';
+
+const logger = getLogger('services.redisService');
+
+function getRedisUrl(config) {
+  return config.redis && config.redis.url;
+}
+
+function getHost(url) {
+  return url.split('@')[1] || url;
+}
+
+const exhastError = new Error('Retry retry_strategy options.total_retry_time exhausted');
+let firstClient: RedisClient;
+
+export function createRedisConnection(config) {
+  const redisUrl = getRedisUrl(config);
+  if (!redisUrl) return;
+
+  const options = {
+    timesConnected: 10,
+    totalRetryTime: 1000 * 60 * 60,
+    reconnectAfterMultiplier: 1000,
+    socketKeepalive: true,
+    socketInitialDelay: 60000,
+    ...config.redis.options
+  };
+
+  options.retry_strategy = function(opts) {
+    if (opts.error && opts.error.code === 'ECONNREFUSED') logger.error(opts.error);
+    if (opts.total_retry_time > options.totalRetryTime) return exhastError;
+    if (opts.times_connected > options.timesConnected) {
+      const msg =
+        'redis error retry_strategy options.times_connected exhausted.' +
+        'Please verify that the redis-server "timeout" config is large enough or disabled(0).' +
+        'Redis-cli:"config get timeout" ';
+      throw new Error(msg);
+    }
+    return;
+  };
+
+  const client = createClient(redisUrl, options);
+  if (!firstClient) firstClient = client;
+  client.on('connect', () => {
+    const socket = client.stream;
+    socket.setKeepAlive(options.socketKeepalive, options.socketInitialDelay);
+    logger.info(`Redis connected ${getHost(redisUrl)}`);
+  });
+  client.on('error', () => logger.error(`Redis disconnected ${getHost(redisUrl)}`));
+
+  return client;
+}
+
+export function getRedis() {
+  if (!firstClient) throw new Error('Redis is not initialized');
+  return firstClient;
+}
---
+++ test/initializers/redis.test.ts
@@ -0,0 +1,53 @@
+const mock = require('mock-require');
+import * as sinon from 'sinon';
+import 'should';
+
+const sandbox = sinon.createSandbox();
+
+describe('Redis connection', function() {
+  const config = {
+    redis: {
+      url: 'redis://localhost:6379/',
+      options: {
+        sample: 'sample'
+      }
+    }
+  };
+  let redis;
+  let connectStub: sinon.SinonSpy;
+  let onStub: sinon.SinonSpy;
+
+  beforeEach(async function() {
+    onStub = sandbox.stub();
+    connectStub = sandbox.stub().returns({ on: onStub });
+    mock('redis', { createClient: connectStub });
+    ({ createRedisConnection: redis } = await import('../../src/initializers/redis'));
+  });
+
+  afterEach(function() {
+    sandbox.restore();
+    mock.stopAll();
+  });
+
+  it('should connect to redis', () => {
+    redis(config);
+    connectStub.args.should.containDeep([
+      [
+        'redis://localhost:6379/',
+        {
+          timesConnected: 10,
+          totalRetryTime: 3600000,
+          reconnectAfterMultiplier: 1000,
+          socketKeepalive: true,
+          socketInitialDelay: 60000,
+          sample: 'sample'
+        }
+      ]
+    ]);
+  });
+
+  it('should not connect to redis', () => {
+    redis({});
+    connectStub.args.should.eql([]);
+  });
+});
