9ad7a409a0c624e15cb77510cf416b7ceb4f60e7
Kyriakos Lesgidis <44189854+klesgidis@users.noreply.github.com>
2020-09-17 16:14:31 +0300

    0.33.5 (#186)
    * Pass configuration to riviere
    
    * 0.33.5

--- examples/simple-example/config.js
+++ examples/simple-example/config.js
@@ -1,9 +1,15 @@
 module.exports = {
   nodeEnv: 'demo',
+  log: {
+    json: true
+  },
   app: {
     name: 'foo'
   },
   cors: {
     publicPrefixes: ['/api/allowAll']
+  },
+  riviere: {
+    bodyKeysRegex: '.*'
   }
 };
