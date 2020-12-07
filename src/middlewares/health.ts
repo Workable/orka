ad79f321424a61aa4b085d032d591b7c68f0ccca
Kyriakos Lesgidis <44189854+klesgidis@users.noreply.github.com>
2020-12-07 10:36:37 +0200

    [ENG-40] Mitigates statSync error for typescript apps (#207)
    * [ENG-40] Mitigates statSync error for typescript apps
    
    * Use DD_VERSION for app version if exists
    
    * 1.2.1

--- src/middlewares/health.ts
+++ src/middlewares/health.ts
@@ -1,20 +1,27 @@
 import { Context } from 'koa';
 import { isHealthy } from '../initializers/rabbitmq';
 
+export enum ConnectionStates {
+  disconnected = 0,
+  connected = 1,
+  connecting = 2,
+  disconnecting = 3,
+  uninitialized = 99
+}
+
 export default async function(ctx: Context, next: () => Promise<null>) {
-  const mongoose = await import('mongoose');
   const { getConnection } = await import('../initializers/mongodb');
 
   // tslint:disable-next-line: no-empty
   const mongoConnection = getConnection(() => {});
   // tslint:disable-next-line: no-empty
   const isRabbitHealthy = isHealthy();
-  if ((!mongoConnection || mongoConnection.readyState === mongoose.Connection.STATES.connected) && isRabbitHealthy) {
+  if ((!mongoConnection || mongoConnection.readyState === ConnectionStates.connected) && isRabbitHealthy) {
     const OrkaBuilder = (await import('../orka-builder')).default;
     ctx.status = 200;
     ctx.body = {
       env: OrkaBuilder.INSTANCE.config.nodeEnv,
-      version: `v${process.env.npm_package_version}`
+      version: process.env.DD_VERSION || `v${process.env.npm_package_version}`
     };
   } else {
     ctx.status = 503;
