e161a76665870f9f00e7ba9f838d2851f989b139
Nikos Kostoulas <kostoulas@workable.com>
2020-12-16 13:13:56 +0200

    fixup! fixup! fixup! fixup! Add rabbitmq in tests

--- src/utils.ts
+++ src/utils.ts
@@ -6,13 +6,9 @@ export const alsSupported = () =>
   nodeVersionGreaterThanEqual('v14.0.0');
 
 export const nodeVersionGreaterThanEqual = (requestedVersion: string, version = process.version) => {
-  const [major, minor, patch] = version
+  const [, major, minor, patch] = version.match(regex).map((n: string) => parseInt(n, 10));
+  const [, requestedMajor, requestedMinor, requestedPatch] = requestedVersion
     .match(regex)
-    .slice(1)
-    .map((n: string) => parseInt(n, 10));
-  const [requestedMajor, requestedMinor, requestedPatch] = requestedVersion
-    .match(regex)
-    .slice(1)
     .map((n: string) => parseInt(n, 10));
 
   if (major > requestedMajor) return true;
