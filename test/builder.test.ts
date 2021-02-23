4b69d6b4d435d7fbd2dd4f8556cf671863b04042
Manos Kouvarakis <kouvarakis@workable.com>
2021-02-23 14:31:44 +0200

    Fix application name configuration SPC-244 (#227)
    * Fix application name configuration
    
    * Add more tests

--- test/builder.test.ts
+++ test/builder.test.ts
@@ -1,3 +1,7 @@
+import builder from '../src/builder';
+import defaults from '../src/default-options';
+import 'should';
+
 describe('builder', function() {
   it('initializes typescript');
 
@@ -12,4 +16,18 @@ describe('builder', function() {
   it('appends middleware koa');
   it('prepends middleware koa');
   it('uses port');
+
+  it('should get app name from config if not set', () => {
+    const staticOptions = { diamorphosis: { configFolder: './examples/simple-example' } };
+    const orkaBuilder = builder(staticOptions);
+    const { appName } = orkaBuilder.options;
+    appName.should.equal('foo');
+  });
+
+  it('should get app name from options.appName if set', () => {
+    const staticOptions = { appName: 'orka', diamorphosis: { configFolder: './examples/simple-example' } };
+    const orkaBuilder = builder(staticOptions);
+    const { appName } = orkaBuilder.options;
+    appName.should.equal('orka');
+  });
 });
