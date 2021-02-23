3981c6106da5a61204a860b6ea6f699af3ed0c35
Manos Kouvarakis <kouvarakis@workable.com>
2021-02-23 14:25:49 +0200

    Add more tests

--- test/builder.test.ts
+++ test/builder.test.ts
@@ -17,10 +17,17 @@ describe('builder', function() {
   it('prepends middleware koa');
   it('uses port');
 
-  it('should get app name from config', () => {
+  it('should get app name from config if not set', () => {
     const staticOptions = { diamorphosis: { configFolder: './examples/simple-example' } };
     const orkaBuilder = builder(staticOptions);
     const { appName } = orkaBuilder.options;
     appName.should.equal('foo');
+  });
+
+  it('should get app name from options.appName if set', () => {
+    const staticOptions = { appName: 'orka', diamorphosis: { configFolder: './examples/simple-example' } };
+    const orkaBuilder = builder(staticOptions);
+    const { appName } = orkaBuilder.options;
+    appName.should.equal('orka');
   });
 });
