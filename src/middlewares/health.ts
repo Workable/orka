7b366fecb46016b7a10ea8aeb5c791030e09ae6e
Manos Kouvarakis <kouvarakis@workable.com>
2021-04-01 11:36:56 +0300

    Introduce health check for redis connection in /health endpoint ENG-79 (#239)
    * Intoduce health check for redis connection in /health endpoint
    
    * Try redis installation in github workflow

--- src/middlewares/health.ts
+++ src/middlewares/health.ts
@@ -1,5 +1,6 @@
 import { Context } from 'koa';
 import { isHealthy } from '../initializers/rabbitmq';
+import { isHealthy as checkRedisHealth } from '../initializers/redis';
 
 export enum ConnectionStates {
   disconnected = 0,
@@ -9,15 +10,21 @@ export enum ConnectionStates {
   uninitialized = 99
 }
 
-export default async function(ctx: Context, next: () => Promise<null>) {
+export default async function (ctx: Context, next: () => Promise<null>) {
   const { getConnection } = await import('../initializers/mongodb');
+
+  const OrkaBuilder = (await import('../orka-builder')).default;
 
   // tslint:disable-next-line: no-empty
   const mongoConnection = getConnection(() => {});
   // tslint:disable-next-line: no-empty
   const isRabbitHealthy = isHealthy();
-  if ((!mongoConnection || mongoConnection.readyState === ConnectionStates.connected) && isRabbitHealthy) {
-    const OrkaBuilder = (await import('../orka-builder')).default;
+  const isRedisHealthy = !OrkaBuilder.INSTANCE.config.healthCheck.redis || checkRedisHealth();
+  if (
+    (!mongoConnection || mongoConnection.readyState === ConnectionStates.connected) &&
+    isRabbitHealthy &&
+    isRedisHealthy
+  ) {
     ctx.status = 200;
     ctx.body = {
       env: OrkaBuilder.INSTANCE.config.nodeEnv,
--- test/middlewares/health.test.ts
+++ test/middlewares/health.test.ts
@@ -4,27 +4,29 @@ import 'should';
 import health from '../../src/middlewares/health';
 import * as mongodb from '../../src/initializers/mongodb';
 import * as rabbitmq from '../../src/initializers/rabbitmq';
+import * as redis from '../../src/initializers/redis';
 import OrkaBuilder from '../../src/orka-builder';
 
 const sandbox = sinon.createSandbox();
 
-describe('Health middleware', function() {
-  let getConnectionStub, isHealthyStub, orkaBuilderStub;
-  beforeEach(function() {
+describe('Health middleware', function () {
+  let getConnectionStub, isHealthyStub, isReadisHealthyStub;
+  beforeEach(function () {
     getConnectionStub = sandbox.stub(mongodb, 'getConnection');
     isHealthyStub = sandbox.stub(rabbitmq, 'isHealthy');
+    isReadisHealthyStub = sandbox.stub(redis, 'isHealthy');
   });
 
-  afterEach(function() {
+  afterEach(function () {
     sandbox.restore();
   });
 
-  it('returns 200 when mongo connection is ok and rabbitmq is healthy', async function() {
+  it('returns 200 when mongo connection is ok and rabbitmq is healthy', async function () {
     const version = process.env.npm_package_version;
     process.env.npm_package_version = '2.44.0';
     const ctx = {} as Context;
     getConnectionStub.returns({ readyState: 1 });
-    OrkaBuilder.INSTANCE = { config: { nodeEnv: 'test' } } as any;
+    OrkaBuilder.INSTANCE = { config: { nodeEnv: 'test', healthCheck: { redis: false } } } as any;
     isHealthyStub.returns(true);
     const next = sandbox.stub();
     await health(ctx, next);
@@ -35,8 +37,9 @@ describe('Health middleware', function() {
     process.env.npm_package_version = version;
   });
 
-  it('returns 503 when mongo connection is down', async function() {
+  it('returns 503 when mongo connection is down', async function () {
     const ctx = {} as Context;
+    OrkaBuilder.INSTANCE = { config: { nodeEnv: 'test', healthCheck: { redis: false } } } as any;
     getConnectionStub.returns({ readyState: 2 });
     const next = sandbox.stub();
     await health(ctx, next);
@@ -44,13 +47,44 @@ describe('Health middleware', function() {
     ctx.status.should.eql(503);
   });
 
-  it('return 503 when rabbitmq is not healthy', async function() {
+  it('return 503 when rabbitmq is not healthy', async function () {
     const ctx = {} as Context;
+    OrkaBuilder.INSTANCE = { config: { nodeEnv: 'test', healthCheck: { redis: false } } } as any;
     getConnectionStub.returns({ readyState: 1 });
     isHealthyStub.returns(false);
     const next = sandbox.stub();
     await health(ctx, next);
     next.called.should.be.true();
     ctx.status.should.eql(503);
+  });
+
+  context('when config.healthCheck.redis: true', () => {
+    it('returns 200 when mongo connection is ok and rabbitmq is healthy and redis is healthy', async function () {
+      const version = process.env.npm_package_version;
+      process.env.npm_package_version = '2.44.0';
+      const ctx = {} as Context;
+      getConnectionStub.returns({ readyState: 1 });
+      OrkaBuilder.INSTANCE = { config: { nodeEnv: 'test', healthCheck: { redis: true } } } as any;
+      isHealthyStub.returns(true);
+      isReadisHealthyStub.returns(true);
+      const next = sandbox.stub();
+      await health(ctx, next);
+      ctx.status.should.eql(200);
+      ctx.body.version.should.eql('v2.44.0');
+      ctx.body.env.should.eql('test');
+      next.called.should.be.true();
+      process.env.npm_package_version = version;
+    });
+
+    it('return 503 when redis is not healthy', async function () {
+      const ctx = {} as Context;
+      getConnectionStub.returns({ readyState: 1 });
+      isHealthyStub.returns(true);
+      isReadisHealthyStub.returns(false);
+      const next = sandbox.stub();
+      await health(ctx, next);
+      next.called.should.be.true();
+      ctx.status.should.eql(503);
+    });
   });
 });
