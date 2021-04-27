b361de5c04ca9d0606338eb38df571c1417f7aa6
Nikos Kostoulas <kostoulas@workable.com>
2021-04-27 15:51:14 +0300

    Keep tests

--- test/initializers/kafka/index.test.ts
+++ test/initializers/kafka/index.test.ts
@@ -5,4 +5,12 @@ describe('kafka init', () => {
   it('should handle the absence of kafka config', async () => {
     await init({} as any);
   });
+
+  it('should not connect if brokers are empty', async () => {
+    await init({ brokers: [] } as any);
+  });
+
+  it('should not connect if brokers have empty string', async () => {
+    await init({ brokers: [''] } as any);
+  });
 });
