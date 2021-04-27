e5c35acd3af0bfe329ebd9f520c13e76214c4f76
Nikos Kostoulas <kostoulas@workable.com>
2021-04-27 15:50:25 +0300

    Revert "Do not try connect to kafka even on empty strings array (#248)"
    This reverts commit e6f045aa0db479e00f33d5b500ae8ea8bea1ac7a.

--- test/initializers/kafka/index.test.ts
+++ test/initializers/kafka/index.test.ts
@@ -5,12 +5,4 @@ describe('kafka init', () => {
   it('should handle the absence of kafka config', async () => {
     await init({} as any);
   });
-
-  it('should not connect if brokers are empty', async () => {
-    await init({ brokers: []} as any);
-  });
-
-  it('should not connect if brokers have empty string', async () => {
-    await init({ brokers: ['']} as any);
-  });
 });
