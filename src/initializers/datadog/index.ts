d023523f219182b4782e712e9d61107100d615c0
Manos Kouvarakis <kouvarakis@workable.com>
2022-12-27 22:57:51 +0200

    Set ddSpan

--- src/initializers/datadog/index.ts
+++ src/initializers/datadog/index.ts
@@ -5,17 +5,9 @@ let tracer;
 export default config => {
   if (process.env.DD_SERVICE && process.env.DD_ENV) {
     tracer = requireInjected('dd-trace').init();
-    tracer.use('http', { client: false });
     tracer.use('koa', {
       blacklist: config?.datadog?.blacklistedPaths,
       blocklist: config?.datadog?.blacklistedPaths,
-      hooks: {
-        request: (span, ctx) => {
-          if (!ctx.req) ctx.req = {};
-          if (!ctx.req._datadog) ctx.req._datadog = {};
-          ctx.req._datadog = { span: span.context()._trace.started[0] };
-        }
-      },
       ...config.datadog
     });
   }
--- src/middlewares/datadog-match-routes.ts
+++ src/middlewares/datadog-match-routes.ts
@@ -1,8 +1,13 @@
 import { Context } from 'koa';
+import { getDatadogTracer } from './../initializers/datadog/index';
 
 export default async function matchedRoute(ctx: Context, next: () => Promise<void>) {
   if (ctx._matchedRoute) {
-    const ddSpan = ctx.req?._datadog?.span;
+    // const ddSpan = ctx.req?._datadog?.span;
+    const span = getDatadogTracer().scope().active();
+    let ddSpan;
+    if (span) ddSpan = span.context()._trace.started[0];
+
     if (ddSpan) {
       ddSpan.setTag('resource.name', `${ctx.request.method} ${ctx._matchedRoute}`);
       ddSpan.setTag('matchedRoute', ctx._matchedRoute);
