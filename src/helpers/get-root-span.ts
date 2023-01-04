95ef48f2e8f08d2d10615824a79a19d790937c75
Manos Kouvarakis <kouvarakis@workable.com>
2023-01-04 12:46:17 +0200

    Log error if anything changes in dd-trace library

--- src/helpers/get-root-span.ts
+++ src/helpers/get-root-span.ts
@@ -1,12 +1,19 @@
 import { getDatadogTracer, isDatadogEnabled } from '../initializers/datadog/index';
+import { getLogger } from '../initializers/log4js';
+
+const logger = getLogger('orka.helpers.get-root-span');
 
 export default function getRootSpan(ctx) {
   if (!isDatadogEnabled()) return;
 
   let ddSpan = ctx.req?._datadog?.span;
   if (!ddSpan) {
-    const activeSpan = getDatadogTracer()?.scope()?.active();
-    if (activeSpan?.context()?._trace?.started.length > 0) ddSpan = activeSpan?.context()?._trace?.started[0];
+    try {
+      const activeSpan = getDatadogTracer()?.scope()?.active();
+      if (activeSpan?.context()?._trace?.started.length > 0) ddSpan = activeSpan?.context()?._trace?.started[0];
+    } catch (e) {
+      logger.error('dd-trace error trying to find root span', e);
+    }
   }
   return ddSpan;
 }
