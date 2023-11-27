a8a71bfb70ed694710b606c8fadc8ec77162ad88
Yannis Gerbessiotis <gerbessiotis@workable.com>
2023-11-27 15:54:21 +0200

    fixes honeybadger configuration

--- src/initializers/honeybadger.ts
+++ src/initializers/honeybadger.ts
@@ -2,6 +2,7 @@ export default async (config) => {
   if (config.honeybadger.apiKey) {
     const honeybadger = await import('@honeybadger-io/js');
     honeybadger.configure({
+      apiKey: config.honeybadger.apiKey,
       environment: config.app.env,
       developmentEnvironments: config.honeybadger.developmentEnvironments
     });
