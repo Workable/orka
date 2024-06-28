fce0064f172d61462153f27f5b92b23f3ecb208a
Nikos Kostoulas <kostoulas@workable.com>
2024-06-28 12:43:49 +0300

    Passthrough HB config

--- src/initializers/honeybadger.ts
+++ src/initializers/honeybadger.ts
@@ -1,10 +1,9 @@
-export default async (config) => {
+export default async config => {
   if (config.honeybadger.apiKey) {
     const honeybadger = await import('@honeybadger-io/js');
     honeybadger.configure({
-      apiKey: config.honeybadger.apiKey,
       environment: config.app.env,
-      developmentEnvironments: config.honeybadger.developmentEnvironments
+      ...config.honeybadger
     });
   }
 };
