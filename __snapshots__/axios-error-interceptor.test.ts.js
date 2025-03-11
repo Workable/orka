e483c08d7285a02a4d8e142f62680dde0662b6ac
Nikos Kostoulas <kostoulas@workable.com>
2025-03-11 12:02:00 +0200

    fixup! Update snapshot after axios update

--- __snapshots__/axios-error-interceptor.test.ts.js
+++ __snapshots__/axios-error-interceptor.test.ts.js
@@ -1,10 +1,10 @@
 exports['axios error interceptor should add context in error object for failed get request 1'] = {
   "name": "AxiosError",
-  "code": "ERR_BAD_RESPONSE",
-  "status": 504,
+  "code": "ERR_BAD_REQUEST",
+  "status": 404,
   "context": {
     "response": {
-      "status": 504,
+      "status": 404,
       "statusText": null,
       "headers": {},
       "data": ""
